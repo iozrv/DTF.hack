9cf52f7c81bdbf91f2cf3faac9a7d1b1f6accb70
victoriasander <dasteegonzo@gmail.com>
2019-11-16 10:31:37 +0300

    Character script

---
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+
+    public float speed;
+    public float jump;
+    public GameObject rayOrigin;
+    public float rayCheckDistance;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        float MoveX = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector3(MoveX * speed, rb.velocity.y, 0);
+
+    }
+}

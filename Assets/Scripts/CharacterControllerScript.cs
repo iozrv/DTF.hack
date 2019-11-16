d1f9f58d6711eb4eee81ee3d23495bb93305e04f
victoriasander <dasteegonzo@gmail.com>
2019-11-16 17:12:54 +0300

    huy

--- Assets/Scripts/CharacterControllerScript.cs
+++ Assets/Scripts/CharacterControllerScript.cs
@@ -6,6 +6,7 @@ public class CharacterControllerScript : MonoBehaviour
 
     public float speed;
     public float jump;
+    public Animator anim;
     public GameObject rayOrigin;
     public float rayCheckDistance;
     Rigidbody2D rb;
@@ -13,11 +14,19 @@ public class CharacterControllerScript : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     void FixedUpdate()
     {
         float MoveX = Input.GetAxis("Horizontal");
+        if (MoveX < 0 || MoveX > 0)
+        {
+            anim.SetBool("isRun", true);
+        }
+        else anim.SetBool("isRun", false);
+
         rb.velocity = new Vector3(MoveX * speed, rb.velocity.y, 0);
 
     }

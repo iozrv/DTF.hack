5be96a5e147c28623e5f6543e02fdf8e06730807
Ridomka <57562750+Ridomka@users.noreply.github.com>
2019-11-16 17:11:28 +0300

    123test

---
+++ Assets/Scripts/ControllerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMovement : MonoBehaviour
+{
+    public float horizontalSpeed;
+    float speedX;
+    public float verticalImpulse;
+    Rigidbody2D rb;
+    bool isGrounded;
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            speedX = -horizontalSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            speedX = horizontalSpeed;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.AddForce(new Vector2(0, verticalImpulse), ForceMode2D.Impulse);
+        }
+        transform.Translate(speedX, 0, 0);
+        speedX = 0;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = true;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            isGrounded = false;
+    }
+
+
+}
